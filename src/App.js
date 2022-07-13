3a3250f561f8e9ebe94a8c110b1c8430c0c0490d
Maxim <96632620+Hoshivsky1@users.noreply.github.com>
2022-07-13 20:04:07 +0300

    lesson1-2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-// import logo from './logo.svg';
+import {Component, StrictMode} from 'react';
 import './App.css';
 
 const Header = () => {
     return <h2>Hello World!</h2>
 }
 
-const Field = () => {
-	return <input placeholder='Type here' type="text" />
-}
+// const Fields = () => {
+// 	const holder = 'Enter here';
+// 	const styledField = {
+// 		width: '300px'
+// 	}
+// 	return <input placeholder={holder}
+// 				  type="text" 
+// 				  style={styledField } />
+// }
 
 function Btn() {
 	const text = 'Log in';
-	const res = () => {
-		return 'Log in';
-	}
-	const p = <p>Log in</p>
-	return <button>{p}</button>
+	const logged = true;
+	return <button>{logged ? 'Enter' : text}</button>
 }
+
+class Fields extends Component {
+	render() {
+		const holder = 'Enter here';
+		const styledField = {
+			width: '300px'
+		}
+		return <input 
+					placeholder={holder}
+					type="text" 
+					style={styledField } />
+	}
+}		
 
 function App() {
   	return (
     	<div className="App">
-			<Header/>
-			<Field/>
+			<StrictMode>
+				<Header/>
+			</StrictMode>
+			<Fields/>
 			<Btn/>
     	</div>
   	);
 }
 
+export {Header};
 export default App;
  
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-const text = 'Hello World';
 
-const elem = (
-    <div>
-        <h2 className='text'>Text: {text}</h2>
-        <label htmlFor="username"></label>
-        <input placeholder='write text...' type="text" id="username"/>
-        <button tabIndex={0}>Click</button>
-    </div>
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/> 
+    <StrictMode>
+        <App/>
+    </StrictMode>
 );
  
